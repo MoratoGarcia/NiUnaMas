61d26fa5872154779fe07749de089a4170e312d8
mauriciovelasco <holigans_corsa@hotmail.comgit config --global user.name Your>
2016-06-09 19:58:03 -0500

    Hola

---
+++ js/js.js
@@ -0,0 +1,78 @@
+
+var contadorAcoso = 0;
+
+function contador(){
+contadorAcoso = contadorAcoso + 1;
+document.querySelector("#acoso").innerHTML = contadorAcoso;
+}
+var contadorViolacion = 0;
+
+function contadorViola(){
+contadorViolacion = contadorViolacion + 1;
+document.querySelector("#violacion").innerHTML = contadorViolacion;
+}
+var contadorViolencia = 0;
+
+function contadorViolen(){
+contadorViolencia = contadorViolencia + 1;
+document.querySelector("#violencia").innerHTML = contadorViolencia;
+}
+var contadorPension = 0;
+
+function contadorPena(){
+contadorPension = contadorPension + 1;
+document.querySelector("#pension").innerHTML = contadorPension;
+}
+
+
+
+var contadorAcoso = 0;
+
+function contadorHoga(){
+contadorHogar = contadorHogar + 1;
+document.querySelector("#hogar").innerHTML = contadorHogar;
+}
+var contadorViolacion = 0;
+
+function contadorTrabaj(){
+contadorTrabajo = contadorTrabajo + 1;
+document.querySelector("#trabajo").innerHTML = contadorTrabajo;
+}
+var contadorViolencia = 0;
+
+function contadorSiti(){
+contadorSitio = contadorSitio + 1;
+document.querySelector("#sitiopublico").innerHTML = contadorSitio;
+}
+
+
+var contadorDesconocido = 0;
+
+function contadorDesconocid(){
+contadorDesconocido = contadorDesconocido + 1;
+document.querySelector("#desconocido").innerHTML = contadorDesconocido;
+}
+var contadorSuperior = 0;
+
+function contadorSuperio(){
+contadorSuperior = contadorSuperior + 1;
+document.querySelector("#superior").innerHTML = contadorSuperior;
+}
+var contadorViolacion = 0;
+
+function contadorFamilia(){
+contadorFamiliar = contadorFamiliar + 1;
+document.querySelector("#familiar").innerHTML = contadorFamiliar;
+}
+var contadorViolencia = 0;
+
+function contadorConocid(){
+contadorConocido = contadorConocido + 1;
+document.querySelector("#conocido").innerHTML = contadorConocido;
+}
+var contadorViolencia = 0;
+
+function contadorRelacio(){
+contadorRelacion = contadorRelacion + 1;
+document.querySelector("#relacion").innerHTML = contadorRelacion;
+}
